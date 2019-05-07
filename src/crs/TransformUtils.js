70b8a432e8f10ddf20b9b3089a9dd3d65445ad56
tuonina <976056042@qq.com>
2019-05-07 21:21:52 +0800

    添加编码员

--- src/crs/TransformUtils.js
+++ src/crs/TransformUtils.js
@@ -13,10 +13,12 @@ import proj4 from 'proj4'
 export const CRS_DEFS = {
   EPSG234: 'EPSG:2343',
   EPSG4544: 'EPSG:4544',
-  WGS84: 'WGS84'
+  WGS84: 'WGS84',
+  EPSG4490:'EPSG:4490'
 };
 proj4.defs(CRS_DEFS.EPSG234, "+proj=tmerc +lat_0=0 +lon_0=105 +k=1 +x_0=500000 +y_0=0 +a=6378140 +b=6356755.288157528 +units=m +no_defs");
 proj4.defs(CRS_DEFS.EPSG4544, '+proj=tmerc +lat_0=0 +lon_0=105 +k=1 +x_0=500000 +y_0=0 +ellps=GRS80 +units=m +no_defs ');
+proj4.defs("EPSG:4490","+proj=longlat +ellps=GRS80 +no_defs");
 
 /**
  * https://epsg.io/2343#
